db65f607296143097fe983c0a735ca75b51fc0ae
Iippo Leppänen <21137181@edu.riveria.fi>
2021-06-03 10:34:48 +0300

    ADD npm express, path index.html app.js

---
+++ src/app.js
@@ -0,0 +1,26 @@
+const path = require("path");
+const express = require("express");
+const forecast = require("./forecast");
+
+const app = express();
+
+const publicDirectory = path.join(__dirname, "../public");
+app.use(express.static(publicDirectory));
+
+app.get("/weather", (req, res) => {
+    if (!req.query.location) {
+        return res.send({
+            error: "Please give a location.",
+        });
+    }
+    forecast(req.query.location, (error, data) => {
+        if (error) {
+            return res.send({ error });
+        }
+        res.send({
+            forecast: data,
+        });
+    });
+});
+
+app.listen(3000);
